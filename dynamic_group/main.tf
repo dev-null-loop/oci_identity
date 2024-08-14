7d72babd10262739f7d41db147d8cfbb0327593d
devnull-loop <dev.null.loop@gmail.com>
2024-08-14 19:40:31 +0300

    validate if dynamic group already exists

--- dynamic_group/main.tf
+++ dynamic_group/main.tf
@@ -1,7 +1,19 @@
+data "oci_identity_dynamic_groups" "these" {
+  compartment_id = var.tenancy_id
+  name           = var.name
+  state          = var.state
+}
+
 resource "oci_identity_dynamic_group" "this" {
-  compartment_id = var.compartment_id
+  compartment_id = var.tenancy_id
   description    = var.description
   matching_rule  = var.matching_rule
   name           = var.name
   freeform_tags  = var.freeform_tags
+  lifecycle {
+    precondition {
+      condition     = length(data.oci_identity_dynamic_groups.these.dynamic_groups) == 0
+      error_message = "Error: cannot create dynamic group ${var.name} - it already exists."
+    }
+  }
 }
--- dynamic_group/variables.tf
+++ dynamic_group/variables.tf
@@ -1,10 +1,10 @@
-variable "compartment_id" {
-  description = "The OCID of the tenancy containing the group."
+variable "tenancy_id" {
+  description = "(Required) The OCID of the tenancy containing the group."
   type        = string
 }
 
 variable "description" {
-  description = "The description you assign to the group during creation. Does not have to be unique, and it's changeable."
+  description = "(Required) The description you assign to the group during creation. Does not have to be unique, and it's changeable."
   type        = string
 
 }
@@ -15,12 +15,24 @@ variable "matching_rule" {
 }
 
 variable "name" {
-  description = "The name you assign to the group during creation. The name must be unique across all groups in the tenancy and cannot be changed."
+  description = "(Required) The name you assign to the group during creation. The name must be unique across all groups in the tenancy and cannot be changed."
   type        = string
 }
 
 variable "freeform_tags" {
-  description = "Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace."
+  description = "(Optional) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace."
   type        = map(string)
   default     = {}
+}
+
+variable "defined_tags" {
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  type        = map(string)
+  default     = null
+}
+
+variable "state" {
+  description = "(Optional) A filter to only return resources that match the given lifecycle state.  The state value is case-insensitive."
+  type        = string
+  default     = "ACTIVE"
 }
