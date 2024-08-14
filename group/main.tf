21ae3f26ab2519b57e42e098cf41fe7ce5bf9e4c
devnull-loop <dev.null.loop@gmail.com>
2024-08-14 19:27:53 +0300

    validate if group exists before creation

--- group/main.tf
+++ group/main.tf
@@ -1,6 +1,19 @@
+data "oci_identity_groups" "these" {
+  compartment_id = var.tenancy_id
+  name           = var.name
+  state          = var.state
+}
+
 resource "oci_identity_group" "this" {
-  compartment_id = var.compartment_id
+  compartment_id = var.tenancy_id
   description    = var.description
   name           = var.name
+  defined_tags   = var.defined_tags
   freeform_tags  = var.freeform_tags
+  lifecycle {
+    precondition {
+      condition     = length(data.oci_identity_groups.these.groups) == 0
+      error_message = "Error: cannot create group ${var.name} - it already exists."
+    }
+  }
 }
