2618e81fb1c36153c0f8618a72ff705fd3dd6591
dev-null-loop <dev.null.loop@gmail.com>
2025-04-08 14:56:32 +0300

    identity:dynamic_group refactoring

--- dynamic_group/main.tf
+++ dynamic_group/main.tf
@@ -1,8 +1,21 @@
-data "oci_identity_dynamic_groups" "these" {
-  compartment_id = var.tenancy_id
-  name           = var.name
-  state          = var.state
+check "health_check" {
+  data "oci_identity_dynamic_groups" "these" {
+    compartment_id = var.tenancy_id
+    name           = var.name
+    state          = var.state
+  }
+
+  assert {
+    condition     = length(data.oci_identity_dynamic_groups.these.dynamic_groups) == 0
+    error_message = "Error: cannot create dynamic group ${var.name} - it already exists."
+  }
 }
+
+# data "oci_identity_dynamic_groups" "these" {
+#   compartment_id = var.tenancy_id
+#   name           = var.name
+#   state          = var.state
+# }
 
 resource "oci_identity_dynamic_group" "this" {
   compartment_id = var.tenancy_id
@@ -10,10 +23,10 @@ resource "oci_identity_dynamic_group" "this" {
   matching_rule  = var.matching_rule
   name           = var.name
   freeform_tags  = var.freeform_tags
-  lifecycle {
-    precondition {
-      condition     = length(data.oci_identity_dynamic_groups.these.dynamic_groups) == 0
-      error_message = "Error: cannot create dynamic group ${var.name} - it already exists."
-    }
-  }
+  # lifecycle {
+  #   precondition {
+  #     condition     = length(data.oci_identity_dynamic_groups.these.dynamic_groups) == 0
+  #     error_message = "Error: cannot create dynamic group ${var.name} - it already exists."
+  #   }
+  # }
 }
