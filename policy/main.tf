2c1c90d2ee57552e13c0c8b081d5b4e8489d2d7c
bogdan-m-darie <bogdan.m.darie@oracle.com>
2025-07-08 11:35:48 +0300

    policy: local-exec for duplicates when provisioning nok

--- policy/main.tf
+++ policy/main.tf
@@ -1,7 +1,31 @@
+data "oci_identity_policies" "these" {
+  compartment_id = var.compartment_id
+  name           = var.name
+  state          = var.state
+}
+
 resource "oci_identity_policy" "this" {
   compartment_id = var.compartment_id
   description    = var.description
   name           = var.name
   statements     = var.statements
+  defined_tags   = var.defined_tags
   freeform_tags  = var.freeform_tags
+  version_date   = var.version_date
+  # The local-exec provisioner invokes a local executable after a resource is created.
+  provisioner "local-exec" {
+    when        = create
+    interpreter = ["/bin/bash", "-c"]
+    command = (
+      length(data.oci_identity_policies.these.policies) > 0 ?
+      "/bin/false" :
+      "/bin/true"
+    )
+  }
+  lifecycle {
+    precondition {
+      condition     = length(data.oci_identity_policies.these.policies) == 0
+      error_message = "Error: cannot create policy ${var.name} - it already exists."
+    }
+  }
 }
--- policy/variables.tf
+++ policy/variables.tf
@@ -1,25 +1,43 @@
 variable "compartment_id" {
-  description = "The OCID of the compartment containing the policy (either the tenancy or another compartment)."
+  description = "(Required) The OCID of the compartment containing the policy (either the tenancy or another compartment)."
   type        = string
 }
 
 variable "description" {
-  description = "The description you assign to the policy during creation. Does not have to be unique, and it's changeable."
+  description = "(Required) The description you assign to the policy during creation. Does not have to be unique, and it's changeable."
   type        = string
 }
 
 variable "name" {
-  description = " The name you assign to the policy during creation. The name must be unique across all policies in the tenancy and cannot be changed."
+  description = "(Required) The name you assign to the policy during creation. The name must be unique across all policies in the tenancy and cannot be changed."
   type        = string
 }
 
 variable "statements" {
-  description = "An array of policy statements written in the policy language"
+  description = "(Required) An array of policy statements written in the policy language"
   type        = list(string)
 }
 
+variable "defined_tags" {
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  type        = map(string)
+  default     = null
+}
+
 variable "freeform_tags" {
-  description = "Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace."
+  description = "(Optional) (Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace."
   type        = map(string)
   default     = {}
+}
+
+variable "version_date" {
+  description = "(Optional) (Updatable) The version of the policy. If null or set to an empty string, when a request comes in for authorization, the policy will be evaluated according to the current behavior of the services at that moment. If set to a particular date (YYYY-MM-DD), the policy will be evaluated according to the behavior of the services on that date."
+  type        = string
+  default     = null
+}
+
+variable "state" {
+  description = "(Optional) A filter to only return resources that match the given lifecycle state.  The state value is case-insensitive."
+  type        = string
+  default     = "ACTIVE"
 }
