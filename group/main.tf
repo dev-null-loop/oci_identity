acb325eaa994eba693758c79c9ec2ef429b55621
bogdan-m-darie <bogdan.m.darie@oracle.com>
2025-07-08 10:44:58 +0300

    group: added duplicate check when provisioning for 1st time

--- group/main.tf
+++ group/main.tf
@@ -10,10 +10,13 @@ resource "oci_identity_group" "this" {
   name           = var.name
   defined_tags   = var.defined_tags
   freeform_tags  = var.freeform_tags
-  lifecycle {
-    precondition {
-      condition     = length(data.oci_identity_groups.these.groups) == 0
-      error_message = "Error: cannot create group ${var.name} - it already exists."
-    }
+  provisioner "local-exec" {
+    when        = create
+    interpreter = ["/bin/bash", "-c"]
+    command = (
+      length(data.oci_identity_groups.these.groups) > 0 ?
+      "/bin/false" :
+      "/bin/true"
+    )
   }
 }
