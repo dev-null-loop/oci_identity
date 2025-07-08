cf5bfdff56bb5c1f08c2d2a7cc2b4ac071a921e9
bogdan-m-darie <bogdan.m.darie@oracle.com>
2025-07-08 10:18:07 +0300

    compartment: updated descriptions

--- compartment/variables.tf
+++ compartment/variables.tf
@@ -1,5 +1,5 @@
 variable "compartment_id" {
-  description = "the ocid of the parent compartment containing the compartment."
+  description = "(Required) (Updatable) The OCID of the parent compartment containing the compartment."
   type        = string
 }
 
@@ -10,18 +10,18 @@ variable "description" {
 }
 
 variable "name" {
-  description = "the name you assign to the compartment during creation. The name must be unique across all compartments in the parent compartment. Avoid entering confidential information."
+  description = "(Required) (Updatable) The name you assign to the compartment during creation. The name must be unique across all compartments in the parent compartment. Avoid entering confidential information."
   type        = string
 }
 
 variable "enable_delete" {
-  description = "Defaults to false. If omitted or set to false the provider will implicitly import the compartment if there is a name collision, and will not actually delete the compartment on destroy or removal of the resource declaration. If set to true, the provider will throw an error on a name collision with another compartment, and will attempt to delete the compartment on destroy or removal of the resource declaration."
+  description = "(Optional) Defaults to false. If omitted or set to false the provider will implicitly import the compartment if there is a name collision, and will not actually delete the compartment on destroy or removal of the resource declaration. If set to true, the provider will throw an error on a name collision with another compartment, and will attempt to delete the compartment on destroy or removal of the resource declaration."
   type        = bool
   default     = false
 }
 
 variable "freeform_tags" {
-  description = "Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace."
+  description = "(Optional) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace."
   type        = map(string)
   default     = {}
 }
